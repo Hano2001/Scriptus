79ef40f6d71a0d7ffd5e4d5510d5244c6714ce84
Harald <Harald.Norlin@yh.nackademin.se>
2021-11-30 11:00:33 +0100

    testing deploy without checklogin

--- client/src/App.js
+++ client/src/App.js
@@ -18,20 +18,16 @@ function App() {
   const [login, setLogin] = useState(false);
   const history = useHistory();
   let url = window.location.pathname;
-  const checkLogin = async () => {
-    const { data } = await axios.get(`${apiUrl}/auth/checklogin`);
-    if (data === true) {
-      setLogin(true);
-    }
-  };
+  // const checkLogin = async () => {
+  //   const { data } = await axios.get(`${apiUrl}/auth/checklogin`);
+  //   if (data === true) {
+  //     setLogin(true);
+  //   }
+  // };
 
   // useEffect(() => {
-  //   history.push("/home");
-  // }, []);
-
-  useEffect(() => {
-    checkLogin();
-  }, [url]);
+  //   checkLogin();
+  // }, [url]);
   return (
     <div>
       <Navbar login={login} />
--- server/routes/CleanupRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const { pdfCleanup } = require("../controllers/cleanupController");
-const router = express.Router();
-router.route("/").get(pdfCleanup);
-
-module.exports = router;
